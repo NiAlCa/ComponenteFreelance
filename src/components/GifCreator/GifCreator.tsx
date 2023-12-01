810892ade8eaa4bb23578be1d861a57473170b1a
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-12-01 19:11:09 +0100

    refactor: refactorizando componente

--- src/components/GifCreator/GifCreator.tsx
+++ src/components/GifCreator/GifCreator.tsx
@@ -1,48 +1,50 @@
 import React, { useState } from 'react';
 import gifshot from 'gifshot';
 import { NFT } from '../../data/LastSongData.mock';
+import styles from "./GifCreator.module.scss";
 
 interface GenerateGifProps {
   selectedFavorites: NFT[];
 }
 
+export const GifCreator: React.FC<GenerateGifProps> = ({ selectedFavorites }) => {
+  const [gifPreview, setGifPreview] = useState<string | null>(null);
 
+  const generateAndDownloadGif = () => {
+    const images = selectedFavorites.map((nft) => nft.image);
 
-  
-  export const GifCreator: React.FC<GenerateGifProps> = ({ selectedFavorites }) => {
-    const [gifPreview, setGifPreview] = useState<string | null>(null);
-  
-    const generateAndDownloadGif = () => {
-      const images = selectedFavorites.map((nft) => nft.image);
-  
-      gifshot.createGIF({ images, gifWidth: 400, gifHeight: 300 }, (obj: gifshot.GifshotResult) => {
+    const interval = 0.5; 
+
+    gifshot.createGIF(
+      { images, gifWidth: 300, gifHeight: 300, interval },
+      (obj: gifshot.GifshotResult) => {
         if (!obj.error && obj.image) {
-          setGifPreview(obj.image); 
+          setGifPreview(obj.image);
         }
-      });
-    };
-  
-    const downloadGif = () => {
-      if (gifPreview) {
-        const a = document.createElement('a');
-        a.href = gifPreview;
-        a.download = 'favorites.gif';
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
       }
-    };
-  
-    return (
-      <div>
-        <button onClick={generateAndDownloadGif}>Generate GIF</button>
-        {gifPreview && (
-          <div>
-            <img src={gifPreview} alt="GIF Preview" />
-            <button onClick={downloadGif}>Download GIF</button>
-          </div>
-        )}
-      </div>
     );
   };
-  
+
+  const downloadGif = () => {
+    if (gifPreview) {
+      const a = document.createElement('a');
+      a.href = gifPreview;
+      a.download = 'favorites.gif';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  };
+
+  return (
+    <div className={styles['gif-creator-container']}>
+      <button className={styles['create-button']} onClick={generateAndDownloadGif}>Generate GIF</button>
+      {gifPreview && (
+        <div className={styles['gif-content']}>
+          <img className={styles['gif-preview']} src={gifPreview} alt="GIF Preview" />
+          <button className={styles['download-button']} onClick={downloadGif}>Download GIF</button>
+        </div>
+      )}
+    </div>
+  );
+};
